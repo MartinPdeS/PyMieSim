886d7fa340163e9885c06702b37ab227993936f8
Martin de Sivry <martin.poinsinet.de.sivry@gmail.com>
2020-11-10 11:24:41 -0500

    up

--- PyMieCoupling/classes/Misc.py
+++ PyMieCoupling/classes/Misc.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
 import matplotlib.ticker as tick
-from PyMieCoupling.functions.converts import CuPy2NumPy
 
 try:
     import cupy as cp
