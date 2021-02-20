6bc16cc2aca2839ddec55e701089b963e79684b4
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-02-19 21:04:48 -0500

    added an, bn, cn, dn to spherical scatterer plus changed docs

--- PyMieSim/cpp/GLMT.cpp
+++ PyMieSim/cpp/GLMT.cpp
@@ -1,5 +1,9 @@
 #include <boost/math/special_functions/bessel_prime.hpp>
-
+#include <pybind11/pybind11.h>
+#include <pybind11/complex.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
+#include <cmath>
 #include <tuple>
 
 
