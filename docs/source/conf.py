bdd2227200d84fb9168a40dfe20a2edaf3df4b46
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-03-14 19:24:20 -0400

    bye bye ai.cs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ autodoc_mock_imports = ['numpy',
                         'sphinx.ext.autodoc']
 
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # -- Project information -----------------------------------------------------
 
