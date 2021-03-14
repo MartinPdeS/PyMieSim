f3c2c2b160be00215ced6265d55de2fe44353158
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-03-13 20:34:21 -0500

    test doc

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -15,12 +15,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-
-print('#### YOLO', os.getcwd())
-
-print('#### YOLO2', os.getcwd('../..'))
-
-
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+print(os.path)
 # -- Project information -----------------------------------------------------
 
 project = 'PyMieSim'
