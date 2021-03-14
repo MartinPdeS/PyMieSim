288602ececf9d24c139b2745eae95c9f55c65f8e
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-03-13 23:50:57 -0500

    fixing doc

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-autodoc_mock_imports = ['numpy', 'pandas', 'matplotlib', 'mayavi', 'scipy']
+autodoc_mock_imports = ['numpy', 'numpydoc', 'pandas', 'matplotlib', 'mayavi', 'scipy']
 
 
 sys.path.insert(0, os.path.abspath('../../'))
