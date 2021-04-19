919528a2b9f032b12c1c7ff8f78fc59ded8ca85e
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-04-19 13:55:29 -0400

    update docs with new files

--- docs/build/html/New-Material-Silver.py
+++
@@ -1,8 +0,0 @@
-from PyMieSim._Material.utils import LoadOnlineSave
-from PyMieSim import Material
-
-LoadOnlineSave(filename='Silver', url='https://refractiveindex.info/data_csv.php?datafile=data/main/Ag/Johnson.yml')
-
-Mat = Material('Silver')
-
-Mat.Plot()
--- docs/build/html/_static/New-Material.py
+++
@@ -1,8 +0,0 @@
-from PyMieSim._Material.utils import LoadOnlineSave
-from PyMieSim import Material
-
-LoadOnlineSave(filename='BK7', url='https://refractiveindex.info/data_csv.php?datafile=data/glass/schott/N-BK7.yml')
-
-Mat = Material('BK7')
-
-Mat.Plot()
