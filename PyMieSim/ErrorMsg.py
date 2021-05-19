3d526df0c41e453f5cd68cdcaa618284dfa539b8
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-05-19 14:40:57 -0400

    added ErrorMsg

--- PyMieSim/ErrorMsg.py
+++ PyMieSim/ErrorMsg.py
@@ -1,12 +1,12 @@
-warning_NAHigh = "High values of NA do not comply with paraxial \
-                  approximation. Value under 0.4 are prefered."
+warning_NAHigh      = "High values of NA do not comply with paraxial \
+                       approximation. Value under 0.4 are prefered."
 
-Error_LPMissing = "The LP mode has not been previously compilated. "
-                  "Please consult the documentation to do so. "
-                  "Doc available at: "
-                  "https://pymiesim.readthedocs.io/en/latest/LPModes.html"
+Error_LPMissing     = "The LP mode has not been previously compilated. \
+                       Please consult the documentation to do so. \
+                       Doc available at: \
+                       https://pymiesim.readthedocs.io/en/latest/LPModes.html"
 
-Error_MeanCentered = "Coupling mode can either be Centered or Mean."
+Error_MeanCentered  = "Coupling mode can either be Centered or Mean."
 
 Error_IndexMaterial = "You should either choose a material or the RI not both."
 
