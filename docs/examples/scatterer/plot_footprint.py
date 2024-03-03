81d625041f39d31848deb88c68baeab3f5404e9d
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-03-02 23:27:20 -0500

    update doc[publish]

--- docs/examples/scatterer/plot_footprint.py
+++ docs/examples/scatterer/plot_footprint.py
@@ -1,58 +1,47 @@
 """
-Scatterer footprint
-===================
+Scatterer Footprint Calculation and Visualization
+=================================================
 
+This example demonstrates how to compute and visualize the footprint of a scatterer using PyMieSim.
 """
 
-# %%
-# Importing the package: PyOptik, PyMieSim
+# Import necessary components from PyMieSim
 from PyMieSim.single.scatterer import Sphere
 from PyMieSim.single.detector import LPMode
 from PyMieSim.single.source import Gaussian
 from PyMieSim.materials import BK7
 
-# %%
-# Defining the source
-# ~~~~~~~~~~~~~~~~~~~
+# Define the Gaussian light source with specified properties
 source = Gaussian(
-    wavelength=1000e-9,
+    wavelength=1e-6,  # 1000 nm
     polarization_value=0,
     polarization_type='linear',
-    optical_power=1,
-    NA=0.3
+    optical_power=1,  # Arbitrary units
+    NA=0.3  # Numerical Aperture
 )
 
-# %%
-# Defining the scatterer
-# ~~~~~~~~~~~~~~~~~~~~~~
+# Create a spherical scatterer with a specified diameter and material
 scatterer = Sphere(
-    diameter=2000e-9,
+    diameter=2e-6,  # 2000 nm
     source=source,
-    material=BK7
+    material=BK7  # Using BK7 glass material
 )
 
-# %%
-# Defining the detector
-# ~~~~~~~~~~~~~~~~~~~~~
+# Define the LPMode detector with specific parameters
 detector = LPMode(
     mode_number="LP21",
     NA=0.3,
-    sampling=200,
+    sampling=200,  # Number of sampling points
     gamma_offset=0,
     phi_offset=0,
-    coupling_mode='Point'
+    coupling_mode='Point'  # Coupling mode
 )
 
-# %%
-# Computing the data
-# ~~~~~~~~~~~~~~~~~~
+# Compute the footprint data using the defined scatterer and detector
 data = detector.get_footprint(scatterer)
 
-# %%
-# Plotting the data
-# ~~~~~~~~~~~~~~~~~
+# Plot the computed footprint data
 figure = data.plot()
 
-_ = figure.show()
-
-# -
+# Display the plot
+figure.show()
