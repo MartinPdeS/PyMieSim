e2e8585439f1a6da5feb1201c8badb28ff880034
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-03-23 18:18:43 -0400

    wip gui [publish]

--- PyMieSim/__main__.py
+++ PyMieSim/__main__.py
@@ -6,7 +6,7 @@ from PyMieSim.gui.main_window import PyMieSimGUI
 
 def main():
     root = tkinter.Tk()
-    root.geometry("800x800")
+    root.geometry("800x850")
     PyMieSimGUI(root)
     root.mainloop()
 
