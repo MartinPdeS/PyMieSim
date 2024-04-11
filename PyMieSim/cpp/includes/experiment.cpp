bd403836a39f52d02b65505d6a830671f034826c
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-04-11 15:42:17 -0400

    version update adding HG and LG [publish]

--- PyMieSim/cpp/includes/experiment.cpp
+++ PyMieSim/cpp/includes/experiment.cpp
@@ -291,55 +291,55 @@ class Experiment
 
             std::vector<double> output_array(full_size);
 
-            #pragma omp parallel for collapse(10)
-            for (size_t w=0; w<array_shape[0]; ++w)
-            for (size_t j=0; j<array_shape[1]; ++j)
-            for (size_t d=0; d<array_shape[2]; ++d)
-            for (size_t i=0; i<array_shape[3]; ++i)
-            for (size_t n=0; n<array_shape[4]; ++n)
-            for (size_t s=0; s<array_shape[5]; ++s)
-            for (size_t na=0; na<array_shape[6]; ++na)
-            for (size_t p=0; p<array_shape[7]; ++p)
-            for (size_t g=0; g<array_shape[8]; ++g)
-            for (size_t f=0; f<array_shape[9]; ++f)
-            {
-                size_t idx = flatten_multi_index({w, j, d, i, n, s, na, p, g, f}, array_shape);
+            // #pragma omp parallel for collapse(10)
+            // for (size_t w=0; w<array_shape[0]; ++w)
+            // for (size_t j=0; j<array_shape[1]; ++j)
+            // for (size_t d=0; d<array_shape[2]; ++d)
+            // for (size_t i=0; i<array_shape[3]; ++i)
+            // for (size_t n=0; n<array_shape[4]; ++n)
+            // for (size_t s=0; s<array_shape[5]; ++s)
+            // for (size_t na=0; na<array_shape[6]; ++na)
+            // for (size_t p=0; p<array_shape[7]; ++p)
+            // for (size_t g=0; g<array_shape[8]; ++g)
+            // for (size_t f=0; f<array_shape[9]; ++f)
+            // {
+            //     size_t idx = flatten_multi_index({w, j, d, i, n, s, na, p, g, f}, array_shape);
 
-                py::array scalar_field = detectorSet.scalar_fields[py::make_tuple(s, py::ellipsis())];
+            //     py::array scalar_field = detectorSet.scalar_fields[py::make_tuple(s, py::ellipsis())];
 
-                SOURCE::State source_state = SOURCE::State(
-                    sourceSet.wavelength[w],
-                    sourceSet.jones_vector[j],
-                    sourceSet.amplitude[w]
-                );
+            //     SOURCE::State source_state = SOURCE::State(
+            //         sourceSet.wavelength[w],
+            //         sourceSet.jones_vector[j],
+            //         sourceSet.amplitude[w]
+            //     );
 
-                SPHERE::State scatterer_state = SPHERE::State(
-                    sphereSet.diameter[d],
-                    sphereSet.material[i][w],
-                    sphereSet.n_medium[n]
-                );
+            //     SPHERE::State scatterer_state = SPHERE::State(
+            //         sphereSet.diameter[d],
+            //         sphereSet.material[i][w],
+            //         sphereSet.n_medium[n]
+            //     );
 
 
-                DETECTOR::State detector_state  = DETECTOR::State(
-                    scalar_field,
-                    detectorSet.NA[na],
-                    detectorSet.phi_offset[p],
-                    detectorSet.gamma_offset[g],
-                    detectorSet.polarization_filter[f],
-                    detectorSet.rotation_angle[s],
-                    detectorSet.coherent,
-                    detectorSet.point_coupling
-                );
+            //     DETECTOR::State detector_state  = DETECTOR::State(
+            //         scalar_field,
+            //         detectorSet.NA[na],
+            //         detectorSet.phi_offset[p],
+            //         detectorSet.gamma_offset[g],
+            //         detectorSet.polarization_filter[f],
+            //         detectorSet.rotation_angle[s],
+            //         detectorSet.coherent,
+            //         detectorSet.point_coupling
+            //     );
 
-                SPHERE::Scatterer scatterer = SPHERE::Scatterer(
-                    scatterer_state,
-                    source_state
-                );
+            //     SPHERE::Scatterer scatterer = SPHERE::Scatterer(
+            //         scatterer_state,
+            //         source_state
+            //     );
 
-                DETECTOR::Detector detector = DETECTOR::Detector(detector_state);
+            //     DETECTOR::Detector detector = DETECTOR::Detector(detector_state);
 
-                output_array[idx] = abs( detector.get_coupling(scatterer) );
-            }
+            //     output_array[idx] = abs( detector.get_coupling(scatterer) );
+            // }
 
             return vector_to_numpy(output_array, array_shape);
         }
@@ -360,56 +360,56 @@ class Experiment
 
             std::vector<double> output_array(full_size);
 
-            #pragma omp parallel for collapse(10)
-            for (size_t w=0; w<array_shape[0]; ++w)
-            for (size_t j=0; j<array_shape[1]; ++j)
-            for (size_t d=0; d<array_shape[2]; ++d)
-            for (size_t i=0; i<array_shape[3]; ++i)
-            for (size_t n=0; n<array_shape[4]; ++n)
-            for (size_t s=0; s<array_shape[5]; ++s)
-            for (size_t na=0; na<array_shape[6]; ++na)
-            for (size_t p=0; p<array_shape[7]; ++p)
-            for (size_t g=0; g<array_shape[8]; ++g)
-            for (size_t f=0; f<array_shape[9]; ++f)
-            {
-                size_t idx = flatten_multi_index({w, j, d, i, n, s, na, p, g, f}, array_shape);
-                py::array scalar_field = detectorSet.scalar_fields[py::make_tuple(s, py::ellipsis())];
+            // #pragma omp parallel for collapse(10)
+            // for (size_t w=0; w<array_shape[0]; ++w)
+            // for (size_t j=0; j<array_shape[1]; ++j)
+            // for (size_t d=0; d<array_shape[2]; ++d)
+            // for (size_t i=0; i<array_shape[3]; ++i)
+            // for (size_t n=0; n<array_shape[4]; ++n)
+            // for (size_t s=0; s<array_shape[5]; ++s)
+            // for (size_t na=0; na<array_shape[6]; ++na)
+            // for (size_t p=0; p<array_shape[7]; ++p)
+            // for (size_t g=0; g<array_shape[8]; ++g)
+            // for (size_t f=0; f<array_shape[9]; ++f)
+            // {
+            //     size_t idx = flatten_multi_index({w, j, d, i, n, s, na, p, g, f}, array_shape);
+            //     py::array scalar_field = detectorSet.scalar_fields[py::make_tuple(s, py::ellipsis())];
 
-                SOURCE::State source_state = SOURCE::State(
-                    sourceSet.wavelength[w],
-                    sourceSet.jones_vector[j],
-                    sourceSet.amplitude[w]
-                );
+            //     SOURCE::State source_state = SOURCE::State(
+            //         sourceSet.wavelength[w],
+            //         sourceSet.jones_vector[j],
+            //         sourceSet.amplitude[w]
+            //     );
 
-                SPHERE::State scatterer_state = SPHERE::State(
-                    sphereSet.diameter[d],
-                    sphereSet.index[i],
-                    sphereSet.n_medium[n]
-                );
+            //     SPHERE::State scatterer_state = SPHERE::State(
+            //         sphereSet.diameter[d],
+            //         sphereSet.index[i],
+            //         sphereSet.n_medium[n]
+            //     );
 
 
-                DETECTOR::State detector_state = DETECTOR::State(
-                    scalar_field,
-                    detectorSet.NA[na],
-                    detectorSet.phi_offset[p],
-                    detectorSet.gamma_offset[g],
-                    detectorSet.polarization_filter[f],
-                    detectorSet.rotation_angle[s],
-                    detectorSet.coherent,
-                    detectorSet.point_coupling
-                );
+            //     DETECTOR::State detector_state = DETECTOR::State(
+            //         scalar_field,
+            //         detectorSet.NA[na],
+            //         detectorSet.phi_offset[p],
+            //         detectorSet.gamma_offset[g],
+            //         detectorSet.polarization_filter[f],
+            //         detectorSet.rotation_angle[s],
+            //         detectorSet.coherent,
+            //         detectorSet.point_coupling
+            //     );
 
-                SPHERE::Scatterer scatterer = SPHERE::Scatterer(
-                    scatterer_state,
-                    source_state
-                );
+            //     SPHERE::Scatterer scatterer = SPHERE::Scatterer(
+            //         scatterer_state,
+            //         source_state
+            //     );
 
-                DETECTOR::Detector detector = DETECTOR::Detector(detector_state);
+            //     DETECTOR::Detector detector = DETECTOR::Detector(detector_state);
 
-                double coupling = detector.get_coupling(scatterer);
+            //     double coupling = detector.get_coupling(scatterer);
 
-                output_array[idx] = abs(coupling);
-            }
+            //     output_array[idx] = abs(coupling);
+            // }
 
             return vector_to_numpy(output_array, array_shape);
         }
