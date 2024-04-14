e411c0cc1d69c4410607169193d4014e6316a564
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-04-14 18:50:51 -0400

    version update pre-release

--- PyMieSim/cpp/includes/definitions.cpp
+++ PyMieSim/cpp/includes/definitions.cpp
@@ -9,8 +9,6 @@
 typedef std::complex<double> complex128;
 
 #define JJ complex128(0.0,1.0)
-
 #define PI (double)3.14159265358979323846264338
-
-double EPSILON0 = 8.854187817620389e-12;
-double C = 299792458.0 ;
+#define EPSILON0 (double)8.854187817620389e-12
+#define C (double)299792458.0
