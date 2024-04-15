4a7901ee45d52dd65054383fa0c077fcfd51e04c
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-04-15 12:47:33 -0400

    version update pre-release

--- PyMieSim/cpp/includes/sphere.cpp
+++ PyMieSim/cpp/includes/sphere.cpp
@@ -23,8 +23,8 @@ namespace SPHERE
     }
 
     void Scatterer::compute_an_bn(){
-        an.resize(max_order);
-        bn.resize(max_order);
+        an.resize(max_order + 2);
+        bn.resize(max_order + 2);
 
         complex128 psi_n, chi_n, psi_1, chi_1, xi_n, xi_nm1;
 
@@ -36,26 +36,22 @@ namespace SPHERE
 
         std::vector<complex128> Dn = VSH::SPHERICAL::compute_dn(nmx, mx);
 
-        double n;
-
         psi_1  = sin(size_parameter);
         chi_1 = cos(size_parameter);
 
         for (size_t order = 1; order < max_order + 1; ++order)
         {
-            n = (double) order;
-
             // Calculate psi and chi (Riccati-Bessel functions)
-            psi_n = +size_parameter * compute_jn(n, size_parameter);
-            chi_n = -size_parameter * compute_yn(n, size_parameter);
+            psi_n = +size_parameter * compute_jn(order, size_parameter);
+            chi_n = -size_parameter * compute_yn(order, size_parameter);
 
             // Complex Riccati-Bessel functions
             xi_n = psi_n - 1.0 * JJ * chi_n;
             xi_nm1 = psi_1 - 1.0 * JJ * chi_1;
 
             // Derivative of the Riccati-Bessel functions
-            derivative_a = Dn[order] / this->index + n / size_parameter;
-            derivative_b = Dn[order] * this->index + n / size_parameter;
+            derivative_a = Dn[order] / this->index + order / size_parameter;
+            derivative_b = Dn[order] * this->index + order / size_parameter;
 
             // Computation of the electric and magnetic multipole coefficients
             an[order - 1] = (derivative_a * psi_n - psi_1) / (derivative_a * xi_n - xi_nm1);
@@ -87,32 +83,32 @@ namespace SPHERE
 
         for (double i = nmx; i > 1; i--)
         {
-            Cnx[i-2] = i - z*z/(Cnx[i-1] + i);
+            Cnx[i-2] = i - z * z / (Cnx[i - 1] + i);
         }
 
-        for (size_t i = 0; i < max_order; i++)
+        for (size_t order = 0; order < max_order; order++)
         {
-            double n = (double) i;
+            double n = (double) order;
 
-            Cnn.push_back(Cnx[i]);
-            jnx.push_back(compute_jn( n+1, x ));
+            Cnn.push_back(Cnx[order]);
+            jnx.push_back(compute_jn(order + 1, x));
 
-            jnmx.push_back(1. / ( compute_jn(n+1, z )));
-            yx.push_back(compute_yn(n+1, x ));
-            hx.push_back(jnx[i] + JJ * yx[i]);
+            jnmx.push_back(1.0 / (compute_jn(order + 1, z)));
+            yx.push_back(compute_yn(order + 1, x));
+            hx.push_back(jnx[order] + JJ * yx[order]);
 
-            b1x.push_back(jnx[i]);
-            y1x.push_back(yx[i]);
-            hn1x.push_back(b1x[i] + JJ * y1x[i]);
+            b1x.push_back(jnx[order]);
+            y1x.push_back(yx[order]);
+            hn1x.push_back(b1x[order] + JJ * y1x[order]);
 
-            ax.push_back(x * b1x[i] - ( n+1 ) * jnx[i]);
-            ahx.push_back(x * hn1x[i] - ( n+1 ) * hx[i]);
+            ax.push_back(x * b1x[order] - (complex128)(order + 1.0) * jnx[order]);
+            ahx.push_back(x * hn1x[order] - (complex128)(order + 1.0) * hx[order]);
 
-            numerator.push_back( jnx[i] * ahx[i] - hx[i] * ax[i] );
-            c_denominator.push_back( ahx[i] - hx[i] * Cnn[i] );
-            d_denominator.push_back( m * m * ahx[i] - hx[i] * Cnn[i] );
-            cn[i] = jnmx[i] * numerator[i] / c_denominator[i] ;
-            dn[i] = jnmx[i] * m * numerator[i] / d_denominator[i] ;
+            numerator.push_back( jnx[order] * ahx[order] - hx[order] * ax[order] );
+            c_denominator.push_back( ahx[order] - hx[order] * Cnn[order] );
+            d_denominator.push_back( m * m * ahx[order] - hx[order] * Cnn[order] );
+            cn[order] = jnmx[order] * numerator[order] / c_denominator[order] ;
+            dn[order] = jnmx[order] * m * numerator[order] / d_denominator[order] ;
         }
     }
 
