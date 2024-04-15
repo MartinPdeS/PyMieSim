d7b7b5a6d62893a937d86ee04517703b5d2699ce
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-04-15 14:36:03 -0400

    version update pre-release

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,8 +1,23 @@
-from PyMieSim.single.scatterer import Sphere, CoreShell
+from PyMieSim.single.scatterer import Sphere
+from PyMieSim.single.source import Gaussian
 
 
 def test_simple():
-    from PyMieSim.binary.Sets import CppCoreShellSet, CppCylinderSet, CppSphereSet
-    assert True is True, 'test passed.'
+    source = Gaussian(
+        wavelength=750e-9,
+        polarization_value=0,
+        polarization_type='linear',
+        optical_power=1,
+        NA=0.3
+    )
+
+    # scatterer = Sphere(
+    #     diameter=100e-9,
+    #     source=source,
+    #     index=1.5,
+    #     n_medium=1.0
+    # )
+
+    # print(scatterer.Qsca)
 
 # -
