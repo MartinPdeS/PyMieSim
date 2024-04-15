852d3a0badfa5dfa51dfcff0453ba90ef5581f08
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-04-15 14:26:44 -0400

    version update pre-release

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -2,6 +2,7 @@ from PyMieSim.single.scatterer import Sphere, CoreShell
 
 
 def test_simple():
+    from PyMieSim.binary.Sets import CppCoreShellSet, CppCylinderSet, CppSphereSet
     assert True is True, 'test passed.'
 
 # -
