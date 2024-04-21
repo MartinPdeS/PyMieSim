d2510a7203a78b9e128889e699a57754e340ae17
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-04-21 12:48:20 -0400

    bug fix for coherent detector and Fibonacci mesh [publish]

--- PyMieSim/cpp/includes/VSH.cpp
+++ PyMieSim/cpp/includes/VSH.cpp
@@ -7,7 +7,7 @@
 namespace VSH{
     namespace SPHERICAL {
 
-        std::vector<complex128> compute_dn(double &&nmx, complex128 &z) //Page 127 of BH
+        std::vector<complex128> compute_dn(double nmx, complex128 z) //Page 127 of BH
         {
           std::vector<complex128> Dn(nmx, 0.0);
 
@@ -30,19 +30,17 @@ namespace VSH{
           taun.push_back( 3.0 * cos(2. * acos(mu) ) );
 
           double n = 0;
-          for (unsigned int i = 2; i < max_order; i++)
+          for (size_t order = 2; order < max_order; order++)
               {
-               n = (double)i;
+               pin.push_back( ( (2. * n + 1.) * mu * pin[order - 1] - (n + 1.) * pin[order - 2] ) / n );
 
-               pin.push_back( ( (2. * n + 1.) * mu * pin[i-1] - (n + 1.) * pin[i-2] ) / n );
-
-               taun.push_back( (n + 1.) * mu * pin[i] - (n + 2.) * pin[i-1] );
+               taun.push_back( (n + 1.) * mu * pin[order] - (n + 2.) * pin[order - 1] );
              }
 
           return std::make_tuple(pin, taun);
         }
 
-        inline void MiePiTau(double mu, unsigned int max_order, complex128 *pin, complex128 *taun)
+        inline void MiePiTau(double mu, size_t max_order, complex128 *pin, complex128 *taun)
         {
           pin[0] = 1.;
           pin[1] = 3. * mu;
@@ -51,50 +49,18 @@ namespace VSH{
           taun[1] = 3.0 * cos(2. * acos(mu) );
 
           double n = 0;
-          for (unsigned int i = 2; i < max_order; i++)
+          for (size_t order = 2; order < max_order; order++)
               {
-               n = (double)i;
+               pin[order] = ( (2. * n + 1.) * mu * pin[order - 1] - (n + 1.) * pin[order - 2] ) / n;
 
-               pin[i] = ( (2. * n + 1.) * mu * pin[i-1] - (n + 1.) * pin[i-2] ) / n;
-
-               taun[i] = (n + 1.) * mu * pin[i] - (n + 2.) * pin[i-1];
+               taun[order] = (n + 1.) * mu * pin[order] - (n + 2.) * pin[order - 1];
              }
-        }
-
-
-
-
-        SphericalCoordinate RDir(double &theta, double &phi)
-        {
-            SphericalCoordinate Output;
-            Output.r     = sin(theta)*cos(phi);
-            Output.phi   = sin(theta)*sin(phi),
-            Output.theta = cos(theta);
-            return Output;
-        }
-
-        SphericalCoordinate ThetaDir(double &theta, double &phi)
-        {
-            SphericalCoordinate Output;
-            Output.r     = cos(theta)*cos(phi);
-            Output.phi   = cos(theta)*sin(phi),
-            Output.theta = -sin(theta);
-            return Output;
-        }
-
-        SphericalCoordinate PhiDir(double &theta, double &phi)
-        {
-            SphericalCoordinate Output;
-            Output.r     = -sin(phi);
-            Output.phi   = cos(phi),
-            Output.theta = 0;
-            return Output;
         }
     }
 
     namespace CYLINDRICAL {
 
-      std::vector<complex128> compute_dn(double &&nmx, complex128 &z) //Page 205 of BH
+      std::vector<complex128> compute_dn(double nmx, complex128 z) //Page 205 of BH
       {
         std::vector<complex128> Dn(nmx, 0.0);
 
