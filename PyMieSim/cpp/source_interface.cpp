4120a8ee348f5962f661465e7e2318f4b5bc4137
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-06-13 15:36:01 -0400

    fix testing 43

--- PyMieSim/cpp/source_interface.cpp
+++ PyMieSim/cpp/source_interface.cpp
@@ -58,7 +58,9 @@ using complex128 = std::complex<double>;
 // Assuming SOURCE namespace contains your classes
 namespace SOURCE {
 
-    constexpr double PI = 3.14159265358979323846;
+    #define PI (double)3.14159265358979323846264338
+    #define EPSILON0 (double)8.854187817620389e-12
+    #define C (double)299792458.0
 
     class BaseSource {
     public:
