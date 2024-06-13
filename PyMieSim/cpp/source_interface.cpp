134a029ba3be274b5762af51e1160d95f95e6b16
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-06-13 15:43:00 -0400

    fix testing 45

--- PyMieSim/cpp/source_interface.cpp
+++ PyMieSim/cpp/source_interface.cpp
@@ -93,6 +93,13 @@ namespace SOURCE {
             : BaseSource(wavelength, jones_vector, 0.0), NA(NA), optical_power(optical_power) {
             }
 
+        double compute_amplitude_from_power()
+        {
+            double omega = 0.61 * wavelength / NA;
+            double area = 3.1415926535 * pow(omega / 2, 2);
+            double intensity = optical_power / area;
+            // this->amplitude = sqrt(2.0 * intensity / (C * EPSILON0));
+        }
     };
 
 } // namespace SOURCE
