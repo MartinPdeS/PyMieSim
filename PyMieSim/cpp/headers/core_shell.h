4b35fd622b2ba25d07be9bb23fd862bddecb389b
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-06-13 13:13:45 -0400

    fix testing 6

--- PyMieSim/cpp/headers/core_shell.h
+++ PyMieSim/cpp/headers/core_shell.h
@@ -19,58 +19,40 @@
             double x_shell;
 
             Scatterer(double wavelength, double amplitude, double core_diameter, double shell_width,
-            complex128 core_index, complex128 shell_index, double medium_index, std::vector<complex128> jones, size_t max_order)
+            complex128 core_index, complex128 shell_index, double medium_index, std::vector<complex128> jones, size_t max_order = 0)
             : BaseSphericalScatterer(wavelength, jones, amplitude, medium_index), core_diameter(core_diameter), shell_width(shell_width),
             core_index(core_index), shell_index(shell_index)
             {
                 this->shell_diameter = this->core_diameter + this->shell_width;
-                apply_medium();
-                compute_size_parameter();
-                this->max_order = max_order;
-                compute_area();
-                compute_an_bn();
-            }
+                this->apply_medium();
+                this->compute_size_parameter();
 
-            Scatterer(double wavelength, double amplitude, double core_diameter, double shell_width,
-            complex128 core_index, complex128 shell_index, double medium_index, std::vector<complex128> jones)
-            : BaseSphericalScatterer(wavelength, jones, amplitude, medium_index), core_diameter(core_diameter), shell_width(shell_width),
-            core_index(core_index), shell_index(shell_index)
-            {
-                this->shell_diameter = this->core_diameter + this->shell_width;
-                apply_medium();
-                compute_size_parameter();
-                this->max_order = get_wiscombe_criterion(this->size_parameter);
-                compute_area();
-                compute_an_bn();
+                if (max_order == 0)
+                    this->max_order = get_wiscombe_criterion(this->size_parameter);
+                else
+                    this->max_order = max_order;
+
+                this->compute_area();
+                this->compute_an_bn();
             }
 
             Scatterer(double core_diameter, double shell_width, complex128 core_index, complex128 shell_index,
-                double medium_index, SOURCE::BaseSource &source, size_t max_order)
+                double medium_index, SOURCE::BaseSource &source, size_t max_order = 0)
             : BaseSphericalScatterer(source, medium_index), core_diameter(core_diameter), shell_width(shell_width),
             core_index(core_index), shell_index(shell_index)
             {
                 this->shell_diameter = this->core_diameter + this->shell_width;
-                apply_medium();
-                compute_size_parameter();
-                this->max_order = max_order;
-                compute_area();
-                compute_an_bn();
+                this->apply_medium();
+                this->compute_size_parameter();
+
+                if (max_order == 0)
+                    this->max_order = get_wiscombe_criterion(this->size_parameter);
+                else
+                    this->max_order = max_order;
+
+                this->compute_area();
+                this->compute_an_bn();
             }
-
-            Scatterer(double core_diameter, double shell_width, complex128 core_index, complex128 shell_index,
-                double medium_index, SOURCE::BaseSource &source)
-            : BaseSphericalScatterer(source, medium_index), core_diameter(core_diameter), shell_width(shell_width),
-            core_index(core_index), shell_index(shell_index)
-            {
-                this->shell_diameter = this->core_diameter + this->shell_width;
-                apply_medium();
-                compute_size_parameter();
-                this->max_order = get_wiscombe_criterion(this->size_parameter);
-                compute_area();
-                compute_an_bn();
-            }
-
-
 
             void apply_medium();
             void compute_cn_dn();
