ca75f2c2bd56df96ddb1f231699c2e6948cb2740
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-07-25 23:04:36 -0400

    update

--- PyMieSim/_version.py
+++ PyMieSim/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.12.6.post1'
+__version__ = version = '1.12.6.post2'
 __version_tuple__ = version_tuple = (1, 12, 6)
