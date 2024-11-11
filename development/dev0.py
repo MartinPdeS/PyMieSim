ccd5ba6bbb268e1f2cf41fb53c17f1ed2b03de85
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-11-11 20:37:45 +0100

    update gh-actions

--- development/dev0.py
+++ development/dev0.py
@@ -8,8 +8,9 @@ import matplotlib.pyplot as plt
 import PyMieScatt
 
 
-wavelength = np.linspace(200, 1200, 100)
-diameter = 50
+wavelength = np.linspace(200, 1200, 10)
+
+diameter = [50, 100, 150]
 n_medium = 1.5
 material = Material.gold
 
@@ -29,21 +30,19 @@ scatterer = Sphere(
 
 experiment = Setup(scatterer=scatterer, source=source)
 
-pymiesim_dataframe = experiment.get('Qsca', 'Qabs', 'Qext', add_units=False).reset_index()
-pymiesim_dataframe.columns = pymiesim_dataframe.columns.get_level_values(0)
-ax = pymiesim_dataframe.plot(y=['Qsca', 'Qabs', 'Qext'], x='source:wavelength', linewidth=4, figsize=(10, 6))
 
-m = Material.gold.compute_refractive_index(wavelength * 1e-9)
-_, qext_pymiescatt, qsca_pymiescatt, qabs_pymiescatt, *_  = PyMieScatt.MieQ_withWavelengthRange(m, diameter, n_medium, wavelength)
+dataframe = experiment.get('Qsca', add_units=False)
+
+data = dataframe.values
 
 
-for name, value in zip(['Qsca', 'Qabs', 'Qext'], [qext_pymiescatt, qabs_pymiescatt, qsca_pymiescatt]):
-    ax.plot(wavelength, value, ls='-', lw=0.5, marker='o', markersize=4, c='black', label=r"$Q_{name}^{PyMieScatt}$")
+data = data.reshape([10, 3])
 
-plt.show()
+print(data.shape)
 
 
 
-
+# /
+# print(res)
 
 
