535b7a69c3559fd69525b64a8ee4766622a1c097
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-11-21 16:07:25 +0100

    update readme documenatiton badge that was failing

--- development/dev1.py
+++ development/dev1.py
@@ -10,6 +10,7 @@ import numpy as np
 
 from PyMieSim.experiment.scatterer import Sphere
 from PyMieSim.experiment.source import Gaussian
+from PyMieSim.experiment.detector import Photodiode
 from PyMieSim.experiment import Setup
 from PyMieSim.units import nanometer, degree, watt, AU, RIU
 
@@ -24,20 +25,34 @@ source = Gaussian(
 # %%
 # Defining the ranging parameters for the scatterer distribution
 scatterer = Sphere(
-    diameter=np.geomspace(6.36, 1000, 1500) * nanometer,
+    diameter=np.linspace(10, 1000, 1000) * nanometer,
     medium_property=1.33 * RIU,
     property=[1.40, 1.45, 1.50] * RIU,
     source=source
 )
 
 # %%
+# Defining the detector to be employed.
+detector = Photodiode(
+    NA=1.2 * AU,
+    phi_offset=[0.0] * degree,
+    gamma_offset=0.0 * degree,
+    sampling=600 * AU,
+    polarization_filter=None
+)
+
+
+
+# %%
 # Defining the experiment setup
-experiment = Setup(scatterer=scatterer, source=source)
+experiment = Setup(scatterer=scatterer, source=source, detector=detector)
 
 # %%
 # Measuring the properties
-dataframe = experiment.get('Qsca', scale_unit=True, drop_unique_level=True)
+dataframe = experiment.get('coupling', scale_unit=True, drop_unique_level=True)
 
+
+print(dataframe[280:])
 # %%
 # Plotting the results
-dataframe.plot_data(x='scatterer:diameter')
+# dataframe.plot_data(x='scatterer:diameter')
