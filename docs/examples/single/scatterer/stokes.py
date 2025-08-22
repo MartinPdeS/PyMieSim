de0427030f7078c397bdac15d85de5d887b0a5de
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-08-22 17:34:09 +0200

    fixing docs

--- docs/examples/single/scatterer/stokes.py
+++ docs/examples/single/scatterer/stokes.py
@@ -7,26 +7,27 @@ This example demonstrates the computation and visualization of the Stokes parame
 
 # %%
 # Importing the package: PyMieSim
+from TypedUnit import ureg
+
 from PyMieSim.single.scatterer import Sphere
 from PyMieSim.single.source import Gaussian
-from PyMieSim.units import nanometer, degree, watt, AU, RIU
 
 # %%
 # Defining the source
 source = Gaussian(
-    wavelength=750 * nanometer,  # 750 nm
-    polarization=10 * degree,  # Right circular polarization
-    optical_power=1 * watt,  # Arbitrary units
-    NA=0.3 * AU  # Numerical Aperture
+    wavelength=750 * ureg.nanometer,  # 750 nm
+    polarization=10 * ureg.degree,  # Right circular polarization
+    optical_power=1 * ureg.watt,  # Arbitrary units
+    NA=0.3 * ureg.AU  # Numerical Aperture
 )
 
 # %%
 # Defining the scatterer
 scatterer = Sphere(
-    diameter=300 * nanometer,  # 300 nm
+    diameter=300 * ureg.nanometer,  # 300 nm
     source=source,
-    medium_property=1.0 * RIU,  # Refractive property of the surrounding medium
-    property=1.4 * RIU  # Refractive property of the scatterer
+    medium_property=1.0 * ureg.RIU,  # Refractive property of the surrounding medium
+    property=1.4 * ureg.RIU  # Refractive property of the scatterer
 )
 
 # %%
