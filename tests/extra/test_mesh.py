af35bcd980bce5df8e2bc6678a6fa086fadb05f4
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-08-24 16:23:06 +0200

    fixing headless testing with manylinux

--- tests/extra/test_mesh.py
+++ tests/extra/test_mesh.py
@@ -18,7 +18,7 @@ def fibonacci_mesh():
     """
     return FibonacciMesh(
         max_angle=np.pi / 4 * ureg.radian,   # Maximum angle (45 degrees in radians)
-        sampling=100 * ureg.AU,             # Number of sample points
+        sampling=100 * ureg.AU,              # Number of sample points
         phi_offset=30 * ureg.degree,         # Offset in the azimuthal angle (in degrees)
         gamma_offset=45 * ureg.degree,       # Offset in the polar angle (in degrees)
         rotation_angle=0 * ureg.degree       # Rotation angle (in degrees)
