023ed3d1ca64eb189b077e5797265df183005abb
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-11-10 22:48:41 +0100

    re adding allocation of libomp

--- tests/extra/test_system_plot.py
+++ tests/extra/test_system_plot.py
@@ -10,8 +10,10 @@ from PyMieSim.single import plot_system
 from math import sqrt
 import matplotlib.pyplot as plt
 
+
 try:
     import pyvista
+    import sys
 
     if sys.platform in ["linux", "linux2"]:
         pyvista.start_xvfb()  # Works only on linux system!
