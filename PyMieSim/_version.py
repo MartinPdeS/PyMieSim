edaa0774c3b14b5b6affb729072ec2a538e234ca
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-01-12 21:00:11 +0100

    WIP: putting pint mechanism onto cpp side

--- PyMieSim/_version.py
+++ PyMieSim/_version.py
@@ -31,4 +31,4 @@ __commit_id__: COMMIT_ID
 __version__ = version = '3.8.5'
 __version_tuple__ = version_tuple = (3, 8, 5)
 
-__commit_id__ = commit_id = 'gb7849a85d'
+__commit_id__ = commit_id = 'g18863bbbc'
