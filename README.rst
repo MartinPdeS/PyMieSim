4e53744c13cae75ae9a8607fa41bab2eafd74fb4
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-03-15 08:35:20 +0100

    update readme

--- README.rst
+++ README.rst
@@ -52,30 +52,37 @@ Below is a short example computing the scattering efficiency of a sphere.
 
 .. code-block:: python
 
-   import numpy as np
-   from TypedUnit import ureg
+    from PyMieSim.experiment.scatterer_set import SphereSet
+    from PyMieSim.experiment.source_set import GaussianSet
+    from PyMieSim.experiment.polarization_set import PolarizationSet
+    from PyMieSim.experiment import Setup
+    from PyMieSim.units import ureg
+    import numpy as np
 
-   from PyMieSim.experiment.scatterer import SphereSet
-   from PyMieSim.experiment.source import GaussianSet
-   from PyMieSim.experiment import Setup
+    polarization = PolarizationSet(
+        angles=[0] * ureg.degree
+    )
 
-   source = GaussianSet(
-       wavelength=np.linspace(400, 1000, 500) * ureg.nanometer,
-       polarization=0 * ureg.degree,
-       optical_power=1e-3 * ureg.watt,
-       NA=0.2 * ureg.AU,
-   )
+    source = GaussianSet(
+        wavelength=np.linspace(400, 2000, 500) * ureg.nanometer,
+        polarization=polarization,
+        optical_power=1e-3 * ureg.watt,
+        numerical_aperture=0.2 * ureg.AU,
+    )
 
-   scatterer = SphereSet(
-       diameter=[200, 300] * ureg.nanometer,
-       property=[4] * ureg.RIU,
-       medium_property=[1] * ureg.RIU,
-       source=source,
-   )
+    scatterer = SphereSet(
+        diameter=[200, 300] * ureg.nanometer,
+        material=[4 + 1j] * ureg.RIU,
+        medium=[1] * ureg.RIU,
+    )
 
-   experiment = Setup(scatterer=scatterer, source=source)
-   df = experiment.get("Qsca")
-   df.plot(x="source:wavelength")
+    experiment = Setup(
+        scatterer_set=scatterer,
+        source_set=source
+    )
+    df = experiment.get("Qsca", "Qext")
+    df.plot(x="source:wavelength")
+
 
 .. image:: https://github.com/MartinPdeS/PyMieSim/raw/master/docs/images/resonances.png
     :width: 1000
--- development/dev0 copy.py
+++ development/dev0 copy.py
@@ -1,44 +1,30 @@
-"""
-S1 S2 Function Computation
-==========================
-
-This example demonstrates how to compute and visualize the S1 and S2 scattering functions using PyMieSim.
-"""
-
-# %%
-# Importing the package: PyMieSim
+from PyMieSim.experiment.scatterer_set import SphereSet
+from PyMieSim.experiment.source_set import GaussianSet
+from PyMieSim.experiment.polarization_set import PolarizationSet
+from PyMieSim.experiment import Setup
 from PyMieSim.units import ureg
-from PyMieSim.single.scatterer import Sphere
-from PyMieSim.single.source import Gaussian, PolarizationState
-from PyMieSim.single.representations import S1S2
+import numpy as np
 
-from PyMieSim.binary.interface_dispersive_material import DispersiveMaterial
-
-
-material_bk7 = DispersiveMaterial(
-    name="BK7",
-    wavelengths=[400, 500, 600, 700, 800] * ureg.nanometer,
-    refractive_indices=[1.5302, 1.5195, 1.5143, 1.5114, 1.5098] * ureg.RIU,
+polarization = PolarizationSet(
+    angles=[0] * ureg.degree
 )
 
-polarization_state = PolarizationState(angle=0 * ureg.degree)
-
-source = Gaussian(
-    wavelength=450 * ureg.nanometer,
-    polarization=polarization_state,
-    optical_power=1 * ureg.watt,
-    numerical_aperture=0.3 * ureg.AU,
+source = GaussianSet(
+    wavelength=np.linspace(400, 2000, 500) * ureg.nanometer,
+    polarization=polarization,
+    optical_power=1e-3 * ureg.watt,
+    numerical_aperture=0.2 * ureg.AU,
 )
 
-scatterer = Sphere(
-    diameter=6 * ureg.nanometer,
-    source=source,
-    medium=1.0 * ureg.RIU,
-    material=material_bk7,
+scatterer = SphereSet(
+    diameter=[200, 300] * ureg.nanometer,
+    material=[4 + 1j] * ureg.RIU,
+    medium=[1] * ureg.RIU,
 )
 
-scatterer.Qsca
-
-s1s2 = S1S2(scatterer=scatterer, sampling=200)  # Specify the number of sampling points
-
-figure = s1s2.plot()
+experiment = Setup(
+    scatterer_set=scatterer,
+    source_set=source
+)
+df = experiment.get("Qsca", "Qext")
+df.plot(x="source:wavelength")
