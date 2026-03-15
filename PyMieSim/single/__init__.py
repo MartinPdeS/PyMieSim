2ea81b3c2e4be0bbf9a0160ccaf3fcf3ab9afe9b
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-03-15 10:11:12 +0100

    fix compilation

--- PyMieSim/single/__init__.py
+++ PyMieSim/single/__init__.py
@@ -1,5 +1,4 @@
-# from PyMieSim import single
-# print(dir(single))
-# print(dir(single.single))
-
-# from .setup import Setup
+from . import detector
+from . import source
+from . import scatterer
+from .setup import Setup
--- PyMieSim/single/representations/__init__.py
+++ PyMieSim/single/representations/__init__.py
@@ -4,4 +4,3 @@ from .stokes import Stokes
 from .spf import SPF
 from .s1s2 import S1S2
 from .footprint import Footprint
-from .base import BaseRepresentation
