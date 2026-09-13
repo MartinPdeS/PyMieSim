8b79075a871afcb34b5b49e48c2715cf217591da
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-09-13 20:37:57 +0200

    Release v5.3.0

--- PyMieSim/_version.py
+++ PyMieSim/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '5.2.2'
-__version_tuple__ = version_tuple = (5, 2, 2)
+__version__ = version = '5.3.0'
+__version_tuple__ = version_tuple = (5, 3, 0)
 
 __commit_id__ = commit_id = None
